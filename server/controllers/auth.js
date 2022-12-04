23eff3ce98d4ff739180803cb699f2f67656aebe
arsalanshaikhh <arsalan4coder@gmail.com>
2022-12-05 01:17:30 +0530

    added jwt and cookies

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,7 @@ import express from "express";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { createError } from "../error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   //   console.log(req.body);
@@ -24,6 +25,15 @@ export const signin = async (req, res, next) => {
     if (!user) return next(createError(404, "User not found"));
     const isCorrect = await bcrypt.compare(req.body.password, user.password);
     if (!isCorrect) return next(createError(400, "Wrong credentials!"));
+    const token = jwt.sign({ id: user._id }, process.env.JWT);
+    const { password, ...others } = user._doc;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
   } catch (err) {
     next(err);
   }
